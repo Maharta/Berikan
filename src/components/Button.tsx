1fc14e58658bac47be7762c4ff7c4fa25e04ede3
Maharta <gedemaharta.dev@gmail.com>
2022-10-22 17:28:53 +0800

    merged transparentbutton component into button with modes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,30 @@
-import { HTMLAttributes } from "react";
-import classes from "./Button.module.css";
+import { ButtonHTMLAttributes } from "react";
 
-export interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
+  mode?: string;
 }
 
-const Button = ({ label, ...props }: ButtonProps) => {
+const Button = ({ label, mode, ...props }: ButtonProps) => {
+  const primaryClasses =
+    "py-4 tracking-widest px-14 w-52 h-14 rounded-3xl active:scale-110";
+  const modeClasses = {
+    default:
+      "bg-primaryDark text-white font-bold border-none hover:bg-primaryLighterDark",
+    transparent:
+      "bg-none border-4 text-primaryDark leading-3 border-primaryDark hover:bg-[#e6eced]",
+  };
+
+  const secondaryClasses =
+    mode === "transparent" ? modeClasses.transparent : modeClasses.default;
+
   return (
-    <button {...props} className={`${classes.button} ${props.className}`}>
+    <button
+      {...props}
+      className={
+        primaryClasses + " " + secondaryClasses + " " + props.className
+      }
+    >
       {label}
     </button>
   );
