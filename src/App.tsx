cd8da2af5900c168ade2af8204737552d7f6f746
Maharta <gedemaharta.dev@gmail.com>
2022-10-22 17:21:59 +0800

    added App.tsx route now uses the protected route wrapper component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,35 @@
+import { useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/PrivateRoute";
 import ContinueRegister from "./pages/auth/ContinueRegister";
 import HomePage from "./pages/auth/HomePage";
 import LoginPage from "./pages/auth/LoginPage";
+import RegisterProfile from "./pages/auth/RegisterProfile";
 import RegisterPage from "./pages/auth/RegisterPage";
 import MainPage from "./pages/main/MainPage";
+import { RootState } from "./store/store";
+import AccountPage from "./pages/auth/AccountPage";
 
 function App() {
+  const user = useSelector((state: RootState) => state.auth.user);
+  console.log("re-render");
   return (
     <main>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
+        <Route
+          element={<ProtectedRoute isAllowed={!user} redirectPath="/main" />}
+        >
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
         <Route path="/register-continue" element={<ContinueRegister />} />
-        <Route path="/main" element={<MainPage />} />
+        <Route path="/register-profile" element={<RegisterProfile />} />
+        {/*Both already has navigation guards built in, preventing jank when user state re-render*/}
+        <Route element={<ProtectedRoute isAllowed={!!user} />}>
+          <Route path="/main" element={<MainPage />} />
+          <Route path="/account" element={<AccountPage />} />
+        </Route>
       </Routes>
     </main>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
         primaryDark: "var(--color-primarydark)",
+        primaryLighterDark: "var(--color-primaryLighterDark)",
         gradientBackground: "var(--color-gradientBackground)",
       },
     },
