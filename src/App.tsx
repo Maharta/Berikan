afdc1bd120c5b9a44d4b05bc7aba1b8499b794da
Maharta <gedemaharta.dev@gmail.com>
2022-10-23 22:30:41 +0800

    handled loading state first time refresh while waiting for firebase auth state change

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,32 @@ import RegisterPage from "./pages/auth/RegisterPage";
 import MainPage from "./pages/main/MainPage";
 import { RootState } from "./store/store";
 import AccountPage from "./pages/auth/AccountPage";
+import { MutatingDots } from "react-loader-spinner";
 
 function App() {
   const user = useSelector((state: RootState) => state.auth.user);
-  console.log("re-render");
+
+  if (user === undefined) {
+    // we don't know if user state is loggedIn or logout yet, will know soon with onAuthStateChanged
+    return (
+      <div className="gradient-background">
+        <div className="centered">
+          <MutatingDots
+            height="100"
+            width="100"
+            color="#4fa94d"
+            secondaryColor="#4fa94d"
+            radius="12.5"
+            ariaLabel="mutating-dots-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <main>
       <Routes>
@@ -24,9 +46,9 @@ function App() {
           <Route path="/register" element={<RegisterPage />} />
         </Route>
         <Route path="/register-continue" element={<ContinueRegister />} />
-        <Route path="/register-profile" element={<RegisterProfile />} />
-        {/*Both already has navigation guards built in, preventing jank when user state re-render*/}
+        {/*ContinueRegister Already has navigation guards built in, preventing redirect to main after user register*/}
         <Route element={<ProtectedRoute isAllowed={!!user} />}>
+          <Route path="/register-profile" element={<RegisterProfile />} />
           <Route path="/main" element={<MainPage />} />
           <Route path="/account" element={<AccountPage />} />
         </Route>
