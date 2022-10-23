d4e780d99d37ad13c729f141f4f9da1bdde0a6d9
Maharta <gedemaharta.dev@gmail.com>
2022-10-23 22:36:27 +0800

    now uses special createAsyncThunk to register to firebase,handled loading state

--- src/pages/auth/ContinueRegister.tsx
+++ src/pages/auth/ContinueRegister.tsx
@@ -1,11 +1,15 @@
-import { FormEvent, useEffect, useState } from "react";
+import { FormEvent, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { firstStepStorage } from "./RegisterPage";
-import TextInput from "../../components/TextInput";
 import { useInput } from "../../hooks/useInput";
-import Button from "../../components/Button";
-import ActionLink from "../../components/ActionLink";
-import { supabase } from "../../helpers/supabaseClient";
+import TextInput from "../../components/base/TextInput";
+import Button from "../../components/base/Button";
+import ActionLink from "../../components/base/ActionLink";
+import { RootState, useAppDispatch } from "../../store/store";
+import { register } from "../../store/auth-thunks";
+import { useSelector } from "react-redux";
+import { MutatingDots } from "react-loader-spinner";
+import { authActions } from "../../store/auth-slice";
 
 const noEmptyValidationFn = (value: string) => {
   return value.trim().length !== 0;
@@ -13,12 +17,16 @@ const noEmptyValidationFn = (value: string) => {
 
 const ContinueRegister = () => {
   const navigate = useNavigate();
+  const { isLoading, error, user } = useSelector(
+    (state: RootState) => state.auth
+  );
 
   const [firstNameState, firstNameProps] = useInput(noEmptyValidationFn);
   const [lastNameState, lastNameProps] = useInput(noEmptyValidationFn);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const isFormvalid = firstNameState.isValid && lastNameState.isValid;
+
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     if (!firstStepStorage.isValid) {
@@ -30,35 +38,54 @@ const ContinueRegister = () => {
     e.preventDefault();
     if (!isFormvalid) return;
 
-    try {
-      setIsLoading(true);
-      const { error, data } = await supabase.auth.signUp({
+    dispatch(
+      register({
         email: firstStepStorage.email,
         password: firstStepStorage.password,
-      });
-
-      if (error) throw error;
-
-      const { error: insertError } = await supabase.from("profiles").insert({
-        id: data.user?.id,
         firstname: firstNameProps.value,
         lastname: lastNameProps.value,
-      });
-
-      if (insertError) throw insertError;
-
-      navigate("/register-profile", {
-        state: { userData: data },
-      });
-    } catch (error) {
-      alert(error);
-    } finally {
-      setIsLoading(false);
-    }
+      })
+    );
   };
+
+  useEffect(() => {
+    if (user) {
+      navigate("/register-profile", {
+        state: {
+          user: user,
+        },
+        replace: true,
+      });
+    }
+  }, [user, navigate]);
+
+  const backPageHandler = () => {
+    dispatch(authActions.resetError());
+  };
+
+  if (isLoading) {
+    return (
+      <div className="gradient-background">
+        <div className="centered">
+          <MutatingDots
+            height="100"
+            width="100"
+            color="#4fa94d"
+            secondaryColor="#4fa94d"
+            radius="12.5"
+            ariaLabel="mutating-dots-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="gradient-background">
+      {error && <h1>{error}</h1>}
       <section className="flex flex-col">
         <h1 className="text-4xl font-medium text-center mt-12 mb-9">
           Sedikit Lagi
@@ -82,7 +109,7 @@ const ContinueRegister = () => {
             {...lastNameProps}
           />
           <Button label="SELESAI" className="mt-6 text-center" />
-          <ActionLink to="/register" className="mt-4">
+          <ActionLink to="/register" onClick={backPageHandler} className="mt-4">
             KEMBALI
           </ActionLink>
         </form>
