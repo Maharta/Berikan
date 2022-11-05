009296938275e872df5d50f8988a4a8584a014f7
Maharta <gedemaharta.dev@gmail.com>
2022-11-05 11:34:49 +0800

    added two validation function for product upload input

---
+++ src/helpers/validation-function/productInputValidation.ts
@@ -0,0 +1,14 @@
+const noemptyValidationFn = (value: string) => {
+  return value.trim().length !== 0;
+};
+
+const descriptionValidationFn = (value: string) => {
+  // need at least 2 words
+  const regexp = /[a-zA-Z]+\s+[a-zA-Z]+/g;
+  if (regexp.test(value)) {
+    return true;
+  }
+  return false;
+};
+
+export { noemptyValidationFn, descriptionValidationFn };
