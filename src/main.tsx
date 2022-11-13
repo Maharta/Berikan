ae91cafab0e751b414a194cd775feb536f7c82d3
Maharta <gedemaharta.dev@gmail.com>
2022-11-13 16:23:19 +0800

    added react router QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,18 @@ import { Provider } from "react-redux";
 import store from "./store/store";
 import "./index.css";
 import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>
 );
