bd30c3ebc62977afbec72b5bad2dc861cd9277ab
Maharta <gedemaharta.dev@gmail.com>
2022-11-13 16:17:39 +0800

    added ImagesPicker component, using ImagePicker and can be customized how many image to pick

---
+++ src/components/ImagesPicker.tsx
@@ -0,0 +1,45 @@
+import { HTMLAttributes, useCallback, useEffect, useState } from "react";
+import ImagePicker from "./ImagePicker";
+
+interface ImagesPickerProps extends HTMLAttributes<HTMLDivElement> {
+  numberOfImages: number;
+  onImagesChanged: (imageArrays: File[]) => void;
+}
+
+const ImagesPicker = ({
+  numberOfImages,
+  onImagesChanged,
+  ...props
+}: ImagesPickerProps) => {
+  const [imageArrays, setImageArrays] = useState<File[]>([]);
+
+  const onAddImageHandler = useCallback(
+    (img: File, index?: number) => {
+      console.log(imageArrays[index!]);
+      console.log(index);
+      const newArray = [...imageArrays];
+      newArray[index!] = img;
+      setImageArrays(newArray);
+    },
+    [imageArrays]
+  );
+
+  useEffect(() => {
+    onImagesChanged(imageArrays);
+  }, [imageArrays, onImagesChanged]);
+
+  return (
+    <div className={props.className}>
+      {Array.from(Array(numberOfImages), (_, index) => (
+        <ImagePicker
+          type="upload"
+          index={index}
+          onAddFile={onAddImageHandler}
+          key={index}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ImagesPicker;
