6287ce14c2f8a119dcdfdf9e1e5a4364c25be0a5
Maharta <gedemaharta.dev@gmail.com>
2022-11-13 16:22:13 +0800

    refactored NavHeader and renamed to NavLayout

---
+++ src/layout/NavLayout.tsx
@@ -0,0 +1,27 @@
+import { ArrowLeftIcon } from "@heroicons/react/24/solid";
+import { Fragment, ReactNode } from "react";
+import { Link } from "react-router-dom";
+
+interface NavHeaderProps {
+  title: string;
+  backTo?: string;
+  children: ReactNode;
+}
+
+const NavLayout = ({ title, backTo = "/", children }: NavHeaderProps) => {
+  return (
+    <Fragment>
+      <header>
+        <nav className="flex items-center gap-3 p-2">
+          <Link to={backTo}>
+            <ArrowLeftIcon className="h-5 w-5 font-bold" />
+          </Link>
+          <h1 className="font-bold">{title}</h1>
+        </nav>
+      </header>
+      <main className="pt-4 font-roboto">{children}</main>
+    </Fragment>
+  );
+};
+
+export default NavLayout;
