912d3b85a035b7e7188763a980e0c275172283da
Maharta <gedemaharta.dev@gmail.com>
2022-11-13 16:21:30 +0800

    now use cva library for variants

--- src/components/base/Button.tsx
+++ src/components/base/Button.tsx
@@ -1,30 +1,33 @@
+import { cva, VariantProps } from "cva";
 import { ButtonHTMLAttributes } from "react";
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
-  mode?: string;
 }
 
-const Button = ({ label, mode, ...props }: ButtonProps) => {
-  const primaryClasses =
-    "py-4 tracking-widest px-14 w-52 h-14 rounded-3xl active:scale-110";
-  const modeClasses = {
-    default:
-      "bg-primaryDark text-white font-bold border-none hover:bg-primaryLighterDark",
-    transparent:
-      "bg-none border-4 text-primaryDark leading-3 border-primaryDark hover:bg-[#e6eced]",
-  };
+const buttonStyles = cva(
+  "py-4 tracking-widest px-14 w-52 h-14 rounded-3xl active:scale-110",
+  {
+    variants: {
+      mode: {
+        primary:
+          "bg-primaryDark text-white font-bold border-none hover:bg-primaryLighterDark",
+        transparent:
+          "bg-none border-4 text-primaryDark leading-3 border-primaryDark hover:bg-[#e6eced]",
+      },
+    },
+    defaultVariants: {
+      mode: "primary",
+    },
+  }
+);
+interface Props extends ButtonProps, VariantProps<typeof buttonStyles> {}
 
-  const secondaryClasses =
-    mode === "transparent" ? modeClasses.transparent : modeClasses.default;
-
+const Button = ({ label, mode, ...props }: Props) => {
   return (
     <button
       {...props}
-      className={
-        primaryClasses + " " + secondaryClasses + " " + props.className
-      }
-    >
+      className={buttonStyles({ mode, class: props.className })}>
       {label}
     </button>
   );
