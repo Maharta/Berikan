1c20300f88d93fba316daeee27cdd0181f00629e
Maharta <gedemaharta.dev@gmail.com>
2022-11-15 01:48:38 +0800

    FEELING GOOD! implemented a really hard infinite scroll!

--- src/components/product/InfinityProduct.tsx
+++ src/components/product/InfinityProduct.tsx
@@ -1,8 +1,7 @@
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import {
   collection,
   DocumentData,
-  getCountFromServer,
   getDocs,
   limit,
   orderBy,
@@ -10,47 +9,48 @@ import {
   query,
   startAfter,
 } from "firebase/firestore";
-import { useState } from "react";
+import React, { Fragment, useCallback, useRef } from "react";
 import { db } from "../../firebase";
+import Product, { FirestoreProduct } from "../../models/product";
 import CardCarousel from "../CardCarousel";
 import ProductItem from "./ProductItem";
+
+const productRef = collection(db, "item");
+const q = query(productRef, orderBy("updated_at", "asc"), limit(5));
 
 const infinityProductFetcher = async (query: Query<DocumentData>) => {
   return await getDocs(query);
 };
 
-const totalProductFetcher = async () => {
-  const productRef = collection(db, "item");
-  const q = query(productRef, orderBy("updated_at", "asc"), limit(10));
-  return await getCountFromServer(q);
-};
-
 const InfinityProduct = () => {
-  const [isInfinityReady, setIsInfinityReady] = useState(false);
-  const productRef = collection(db, "item");
-  const q = query(productRef, orderBy("updated_at", "asc"), limit(10));
-
-  const {
-    data: countResponse,
-    isLoading,
-    error,
-    isError,
-    isSuccess,
-  } = useQuery({
-    queryKey: ["products-count"],
-    queryFn: totalProductFetcher,
-  });
-
-  useInfiniteQuery({
+  const { data, isLoading, isError, error, fetchNextPage } = useInfiniteQuery({
     queryKey: ["products"],
     queryFn: ({ pageParam = q }) => infinityProductFetcher(pageParam),
-    getNextPageParam: (lastPageSnapshot, allPages) => {
-      const totalProduct = countResponse?.data().count;
-      const totalPage = totalProduct / 10;
+    getNextPageParam: (lastPageSnapshot) => {
+      if (!lastPageSnapshot.docs.length) return;
+      const lastDocument =
+        lastPageSnapshot.docs[lastPageSnapshot.docs.length - 1];
+      return query(q, startAfter(lastDocument));
     },
   });
 
-  console.log(countResponse?.data().count);
+  const isFetchingRef = useRef(false); // ref for stopping calls to API for infinite scroll if currently fetching
+  const onScrollHandler = useCallback(
+    async (e: React.UIEvent<HTMLDivElement>) => {
+      const { scrollWidth, scrollLeft, clientWidth } =
+        e.target as HTMLDivElement;
+      if (scrollWidth - scrollLeft <= clientWidth * 1.3) {
+        if (isFetchingRef.current === false) {
+          isFetchingRef.current = true;
+          await fetchNextPage();
+          isFetchingRef.current = false;
+        } else {
+          return;
+        }
+      }
+    },
+    [fetchNextPage]
+  );
 
   if (isLoading) {
     return <div>Loading..</div>;
@@ -63,12 +63,36 @@ const InfinityProduct = () => {
     return <div>Something went wrong</div>;
   }
 
+  let products;
+  if (data) {
+    let tempArray: Product[] = [];
+    data.pages.forEach((itemSnapshot) => {
+      itemSnapshot.forEach((item) => {
+        const itemData = item.data() as FirestoreProduct;
+        const transformedData = {
+          ...itemData,
+          id: item.id,
+          updated_at: itemData.updated_at.toDate(),
+        };
+        tempArray.push(transformedData);
+      });
+    });
+    products = tempArray;
+    console.log(products);
+  }
+
   return (
-    <CardCarousel>
-      <div>.</div>
-      <div>.</div>
-      <div>.</div>
-    </CardCarousel>
+    <Fragment>
+      <CardCarousel onScroll={onScrollHandler}>
+        {products?.map((product) => (
+          <ProductItem
+            className="snap-start last:snap-end"
+            key={product.id}
+            product={product}
+          />
+        ))}
+      </CardCarousel>
+    </Fragment>
   );
 };
 
