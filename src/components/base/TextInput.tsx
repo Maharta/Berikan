2e39a7e20fd7aef24dd35cffd81c06c1c6fabcb3
Maharta <gedemaharta.dev@gmail.com>
2022-11-15 15:50:33 +0800

    TextInput component now uses tailwind instead of css modules

--- src/components/base/TextInput.tsx
+++ src/components/base/TextInput.tsx
@@ -1,28 +1,39 @@
-import { HTMLProps } from "react";
-import classes from "./TextInput.module.css";
+import { cva } from "cva";
+import { InputHTMLAttributes } from "react";
 
-interface TextInputProps extends HTMLProps<HTMLInputElement> {
+interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
   id: string;
   type: string;
   label: string;
   isInvalid: boolean;
 }
 
+const inputStyles = cva(
+  "border-[1.2] mx-auto block w-[80%] max-w-xs border-black p-2"
+);
+
 const TextInput = ({
   id,
   label,
   type,
   isInvalid,
+  className,
   ...inputProps
 }: TextInputProps) => {
-  const inputClasses = isInvalid
-    ? `${classes.input} ${classes.invalid}`
-    : classes.input;
-
   return (
-    <div className={inputClasses}>
-      <label htmlFor={id}>{label}</label>
-      <input {...inputProps} type={type} id={id} />
+    <div className="w-full ">
+      <label className="my-0 mx-auto block w-[80%] max-w-xs" htmlFor={id}>
+        {label}
+      </label>
+      <input
+        style={{
+          border: isInvalid ? "2px solid red" : "",
+        }}
+        {...inputProps}
+        className={inputStyles({ class: className })}
+        type={type}
+        id={id}
+      />
     </div>
   );
 };
