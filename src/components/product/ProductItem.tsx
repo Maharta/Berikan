b8e8a3b96c94a362b810bbb300118705408ee9a7
Maharta <gedemaharta.dev@gmail.com>
2022-11-15 18:23:58 +0800

    minor style fix for ProductItem, now use absolute positioning for captions.

--- src/components/product/ProductItem.tsx
+++ src/components/product/ProductItem.tsx
@@ -9,18 +9,22 @@ interface ProductItemProps extends HTMLAttributes<HTMLDivElement> {
 
 const ProductItem = ({ product, ...props }: ProductItemProps) => {
   return (
-    <Card key={product.id} className={`mb-3 rounded-lg ${props.className}`}>
+    <Card
+      key={product.id}
+      className={`relative mb-3 rounded-lg ${props.className}`}>
       <figure>
         <img
           src={product.images[0]}
           className="aspect-square w-full rounded-lg rounded-b-none object-cover"
           alt={product.description}
         />
-        <figcaption className="pl-1 text-lg">{product.name}</figcaption>
+        <figcaption className="pb-10 pl-1 text-lg font-[500]">
+          {product.name}
+        </figcaption>
       </figure>
-      <div className="mt-4 flex justify-between px-1 text-sm uppercase text-slate-500">
+      <div className="absolute bottom-0 flex w-full justify-between px-1 text-sm uppercase text-slate-500">
         <address className="not-italic">{product.location}</address>
-        <span>{`${timeSince(product.updated_at, "id")} lalu`}</span>
+        <span>{`${timeSince(product.updated_at!, "id")} lalu`}</span>
       </div>
     </Card>
   );
