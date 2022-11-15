5b15b0148e131c7ba82d6f0cb6527c1c71805b16
Maharta <gedemaharta.dev@gmail.com>
2022-11-15 18:28:05 +0800

    mounted the InfinityProduct component to MainPage

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -3,19 +3,23 @@ import FloatingButton, {
   HorizontalPosition,
   VerticalPosition,
 } from "../../components/FloatingButton";
+import InfinityProduct from "../../components/product/InfinityProduct";
 import NewProducts from "../../components/product/NewProducts";
-import MainHeader from "../../layout/MainHeader";
+import MainLayout from "../../layout/MainLayout";
 
 const MainPage = () => {
   return (
     <>
-      <MainHeader />
-      <main className="px-4">
+      <MainLayout>
         <section aria-label="baru ditambahkan">
           <h1 className="mb-3 text-xl font-bold">Baru Ditambahkan</h1>
           <NewProducts />
         </section>
-      </main>
+        <section aria-label="semua barang">
+          <h1 className="text-xl font-bold">Semua Barang</h1>
+          <InfinityProduct />
+        </section>
+      </MainLayout>
       <FloatingButton
         horizontal={HorizontalPosition.Right}
         vertical={VerticalPosition.Bottom}
