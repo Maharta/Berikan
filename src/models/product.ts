45d327f8277dcba5e0a6198a1a671fc1a5c63244
Maharta <gedemaharta.dev@gmail.com>
2022-11-15 18:27:15 +0800

    Simplified Product type

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,6 +1,6 @@
 import { Timestamp } from "firebase/firestore";
 
-interface ProductArgs {
+interface Product {
   id: string;
   name: string;
   description: string;
@@ -12,45 +12,11 @@ interface ProductArgs {
     lng: number;
   };
   location: string;
+  address: string;
 }
 
-export interface FirestoreProduct
-  extends Omit<ProductArgs, "id" | "updated_at"> {
+export interface FirestoreProduct extends Omit<Product, "id" | "updated_at"> {
   updated_at: Timestamp;
-}
-
-class Product {
-  id: string;
-  name: string;
-  description: string;
-  updated_at: Date;
-  images: string[];
-  ownerId: string;
-  position: {
-    lat: number;
-    lng: number;
-  };
-  location: string;
-
-  constructor({
-    id,
-    name,
-    description,
-    updated_at = new Date(),
-    images,
-    ownerId,
-    position,
-    location,
-  }: ProductArgs) {
-    this.id = id;
-    this.name = name;
-    this.description = description;
-    this.updated_at = updated_at;
-    this.images = images;
-    this.ownerId = ownerId;
-    this.position = position;
-    this.location = location;
-  }
 }
 
 export default Product;
