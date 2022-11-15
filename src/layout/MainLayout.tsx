f5c456efc3f8a904d5db3d43fbd185cb9e1a944e
Maharta <gedemaharta.dev@gmail.com>
2022-11-15 18:24:28 +0800

    Renamed MainHeader to MainLayout, now is usable like a layout component

---
+++ src/layout/MainLayout.tsx
@@ -0,0 +1,47 @@
+import {
+  ChatBubbleLeftIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/24/outline";
+import { UserCircleIcon } from "@heroicons/react/24/outline";
+import { Fragment, ReactNode } from "react";
+import { NavLink } from "react-router-dom";
+
+interface MainHeaderProps {
+  children: ReactNode;
+}
+
+const MainLayout = ({ children }: MainHeaderProps) => {
+  return (
+    <Fragment>
+      <header className="flex justify-start p-2">
+        <form className="mr-auto flex w-max items-center rounded-full border border-black">
+          <MagnifyingGlassIcon className="ml-3 h-5 w-5" />
+          <input
+            type="search"
+            name="search"
+            id="search"
+            placeholder="Cari di sini."
+            className="ml-2 mr-1 h-full rounded-full outline-none"
+          />
+        </form>
+        <nav>
+          <ul className="flex items-center gap-2">
+            <li>
+              <button>
+                <ChatBubbleLeftIcon className="h-6 w-6" />
+              </button>
+            </li>
+            <li>
+              <NavLink to="/account">
+                <UserCircleIcon className="h-6 w-6" />
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main className="px-4">{children}</main>
+    </Fragment>
+  );
+};
+
+export default MainLayout;
