e2ff257adcb57e2ff7880f37c5822ce57499afd3
Maharta <gedemaharta.dev@gmail.com>
2022-12-01 18:20:45 +0800

    ProductItem is now clickable to see detail of the product.

--- src/components/product/ProductItem.tsx
+++ src/components/product/ProductItem.tsx
@@ -1,18 +1,56 @@
-import { HTMLAttributes } from "react";
+import { HTMLAttributes, useState } from "react";
+import { useSelector } from "react-redux";
+import { useLocation, useNavigate } from "react-router-dom";
 import timeSince from "../../helpers/date/time-since";
 import Product from "../../models/product";
+import { RootState } from "../../store/store";
 import Card from "../base/Card";
+import classNames from "classnames";
+import styles from "./ProductItem.module.css";
 
 interface ProductItemProps extends HTMLAttributes<HTMLDivElement> {
   product: Product;
 }
 
 const ProductItem = ({ product, ...props }: ProductItemProps) => {
+  const currentUser = useSelector((state: RootState) => state.auth.user);
+  const [isOptionOpen, setIsOptionOpen] = useState(true);
+  const navigate = useNavigate();
+
+  const { pathname: currentPath } = useLocation();
+
+  const isDeleteable =
+    currentUser?.uid === product.ownerId &&
+    currentPath === "/account/my-products";
+
+  const onClickHandler = () => {
+    navigate(`/product/${product.id}`, {
+      state: product,
+    });
+  };
+
+  console.log(isDeleteable);
   return (
     <Card
+      onClick={onClickHandler}
       key={product.id}
-      className={`relative mb-3 rounded-lg ${props.className}`}>
+      className={classNames(
+        `relative mb-3 cursor-pointer rounded-lg ${props.className}`,
+        {
+          "hover:scale-105": !isDeleteable,
+        }
+      )}>
       <figure>
+        {isDeleteable && (
+          <button
+            onClick={(e) => e.stopPropagation()}
+            className={styles["dots-container"]}></button>
+        )}
+        {isOptionOpen && (
+          <div className="absolute top-[50px] right-0 w-[150px] bg-white">
+            <button className="p-2">Hapus</button>
+          </div>
+        )}
         <img
           src={product.images[0]}
           className="aspect-square w-full rounded-lg rounded-b-none object-cover"
---
+++ src/pages/main/ProductDetailPage.tsx
@@ -0,0 +1,90 @@
+import { useQuery } from "@tanstack/react-query";
+import { useLocation } from "react-router-dom";
+import ImageCarousel from "../../components/ImageCarousel";
+import LeafletMap from "../../components/map/LeafletMap";
+import OwnerAvatar from "../../components/product/OwnerAvatar";
+import NavLayout from "../../layout/NavLayout";
+import Account from "../../models/account";
+import Product from "../../models/product";
+import accountFetcher from "../../helpers/firebase/accountFetcher";
+
+const ProductDetailPage = () => {
+  const location = useLocation();
+  const product = location.state as Product;
+
+  const images = product.images.map((image) => {
+    return {
+      name: product.name,
+      url: image,
+    };
+  });
+
+  const {
+    isLoading,
+    data: snapshot,
+    error,
+    isError,
+  } = useQuery({
+    queryKey: ["product-owner", product.ownerId],
+    queryFn: () => accountFetcher(product.ownerId),
+  });
+
+  if (isLoading) {
+    return <div>Loading..</div>;
+  }
+
+  if (isError) {
+    if (error instanceof Error) {
+      return <div>{error.message}</div>;
+    }
+    return <div>Something went wrong..</div>;
+  }
+
+  let ownerData;
+  if (snapshot) {
+    ownerData = snapshot.data() as Account;
+  }
+
+  return (
+    <NavLayout paddingBot="0" paddingTop="0.5rem" title="Detail Barang">
+      <ImageCarousel className="h-72 w-full" images={images} />
+      <div className="mt-2 px-2">
+        <h1 className="text-lg font-bold">{product.name}</h1>
+        <p>{product.description}</p>
+        <div className="mb-3">
+          <label className="mb-1 block text-lg font-bold" htmlFor="map">
+            Peta Barang
+          </label>
+          <LeafletMap
+            editable={false}
+            id="map"
+            position={product.position}
+            popupText={product.address}
+          />
+        </div>
+        {ownerData && (
+          <section
+            aria-label="Detail Iklan"
+            className="card grid grid-cols-3 grid-rows-3 rounded-lg pt-2 pb-4">
+            <div className="col-span-1 row-span-2 grid place-content-center">
+              <OwnerAvatar imgUrl={ownerData.avatar_url} />
+            </div>
+            <div className="col-span-2 self-end">
+              Diiklankan {product.updated_at?.toISOString().split("T")[0]}
+            </div>
+            <div className="col-span-2 self-start">
+              {ownerData.firstname + " " + ownerData.lastname}
+            </div>
+            <div className="col-span-3 self-center text-center">
+              <button className="bg-blue-300 px-4 py-2  text-white">
+                Chat Pengiklan
+              </button>
+            </div>
+          </section>
+        )}
+      </div>
+    </NavLayout>
+  );
+};
+
+export default ProductDetailPage;
